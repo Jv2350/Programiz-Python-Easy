abec6d6b888d2e787cbdf2f7fc94a45b3750bd40
Jayesh <116382728+Jv2350@users.noreply.github.com>
2024-06-22 23:30:20 +0530

    Create Forbidden Letter.py

---
+++ Forbidden Letter.py
@@ -0,0 +1,7 @@
+# Write a function to check if a given letter is present in a given string.
+# Instructions
+# Return True if the letter is present, otherwise return False.
+
+
+def check_letter(string, letter):
+    return True if letter in string else False
